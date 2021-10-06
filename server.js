7ca024535a77d9639a56106089cf1b4f33b33c1f
mac <mac@macs-MacBook-Air.local>
2021-10-07 00:48:16 +0700

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-// const cors = require('cors')
-// app.use(cors())
+//moisua
+const cors = require('cors')
+app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
